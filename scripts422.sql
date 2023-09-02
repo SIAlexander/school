ce57b3f44e544c24d3252ba9a81e4376268d8fc8
Smokvin <alex.smokvin@gmail.com>
2023-09-02 13:23:00 +0300

    lesson 4.2 done correct

--- scripts422.sql
+++ scripts422.sql
@@ -1,15 +1,15 @@
 CREATE TABLE cars
 (
-    id    INTEGER PRIMARY KEY,
-    stamp TEXT  NOT NULL UNIQUE,
-    model TEXT  NOT NULL UNIQUE,
+    id    SERIAL PRIMARY KEY,
+    stamp VARCHAR  NOT NULL UNIQUE,
+    model VARCHAR  NOT NULL UNIQUE,
     cost  money NOT NULL
 );
 
 CREATE TABLE humans
 (
-    id     INTEGER PRIMARY KEY,
-    name   TEXT,
+    id     SERIAL PRIMARY KEY,
+    name   VARCHAR NOT NULL UNIQUE,
     age    INTEGER CHECK (age >= 18),
     rights BOOLEAN NOT NULL,
     car_id INTEGER NOT NULL REFERENCES cars (id)
